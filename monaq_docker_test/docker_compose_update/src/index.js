b6b104dac62c4ff2c1c3c4d8272f5f4b733cff4f
monica <monica.kwon@kakaocorp.com>
2018-02-06 16:50:03 +0900

    nginx-mongo-express compose 파일 테스트 업뎃.

---
+++ monaq_docker_test/docker_compose_update/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Todo = require('./todo-model');
+
+mongoose.connect('mongodb://mongo/node');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/todo', function (req, res) {
+  Todo.find()
+    .exec()
+    .then(todos => res.send('hello world'))
+    .catch(err => res.send(err));
+});
+
+app.post('/todo', function (req, res) {
+  const todo = new Todo(req.body);
+  todo.save()
+    .then(() => res.status(201).end())
+    .catch(err => res.status(400).end());
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
