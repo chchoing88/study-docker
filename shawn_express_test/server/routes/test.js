e4cc8e7fd6fec556654ac2e4627a2b76a62a3567
shawn.thecool <shawn.thecool@kakaocorp.com>
2018-03-03 21:28:32 +0900

    shawn. update

--- shawn_express_test/server/routes/test.js
+++ shawn_express_test/server/routes/test.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 const router  = express.Router();
 
 router.get( '/', ( req, res ) => {
-	res.render( 'pages/index', { msg: 'hello index' } );
+	res.render( 'pages/index', { msg: 'hello test' } );
 } );
 
 module.exports = router;
