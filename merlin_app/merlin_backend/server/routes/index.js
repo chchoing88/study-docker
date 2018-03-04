ac323d05e9af2936a8e00098b9251b36342b3347
merlin.ho <merlin.ho@kakaocorp.com>
2018-03-05 01:06:40 +0900

    config merlin frontend env setting

---
+++ merlin_app/merlin_backend/server/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.render( 'pages/merlin', { msg: 'hello index' } );
+});
+
+module.exports = router;
--- merlin_app/merlin_backend/server/server.js
+++ merlin_app/merlin_backend/server/server.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const mongoose            = require( 'mongoose' );
+const path                = require( 'path' );
+const expressHbs          = require( 'express-handlebars' );
+const bodyParser          = require( 'body-parser' );
+const router              = require( './routes' );
 const database            = mongoose.connection;
 
 // 상수
@@ -10,9 +14,27 @@ const HOST = '0.0.0.0';
 
 // 앱
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+let viewPath = path.join( __dirname, 'views' );
+
+//뷰 엔진 설정
+app.set( 'view engine', '.hbs' );
+app.set( 'views', viewPath );
+app.engine( '.hbs', expressHbs({
+	  defaultLayout: 'index',
+	  extname      : '.hbs',
+	  layoutsDir   : path.join( viewPath, 'layouts' ),
+	  partialsDir  : path.join( viewPath, 'partials' )
+  })
+);
+
+//미들웨어 설정
+app.use( bodyParser.json() ); // for parsing application/json
+app.use( bodyParser.urlencoded( {
+	  extended: false
+  }) 
+); // for parsing application x-www-form-urlencoded
+app.use('/', router);
+
 
 //데이터베이스 설정
 database.on( 'error', console.error );
