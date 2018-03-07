cf7934df1abe010a33488af78177318fca155e4a
merlin.ho <merlin.ho@kakakocorp.com>
2018-03-07 18:04:39 +0900

    fixed untracked files

--- merlin_app/merlin_frontend/packages/react-web-component/src/App.js
+++ merlin_app/merlin_frontend/packages/react-web-component/src/App.js
@@ -1,17 +1,12 @@
-import React, { Component } from "react"
-import { hot } from "react-hot-loader"
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 
 // const App = () => <div>Hello World!</div>
 
-
 // export default hot(module)(App)
 class App extends Component {
-	render() {
-		return (
-			<div>
-        Wow merlin React!!!
-			</div>
-		)
-	}
+  render() {
+    return <div>Wow merlin React!!!</div>;
+  }
 }
-export default App
+export default App;
