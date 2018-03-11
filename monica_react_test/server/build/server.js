0efeadc86d51d9778a56ea627881331c5e2f27a6
monica <monica.kwon@kakaocorp.com>
2018-03-11 21:57:19 +0900

    node module ignore

--- monica_react_test/server/build/server.js
+++ monica_react_test/server/build/server.js
@@ -4,6 +4,10 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _post = require('./routes/post');
+
+var _post2 = _interopRequireDefault(_post);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
@@ -13,10 +17,13 @@ var port = 4001;
 // 경로 '/' 로 들어오는 요청들은 public 폴더로 정적 라우팅합니다.
 app.use('/', _express2.default.static(__dirname + '/../public'));
 
-app.get('/api/hello', function (req, res) {
-  var data = [{ msg: 'what!' }, { msg: 'Welcome!' }];
-  res.send(data);
+app.get('/hello', function (req, res) {
+  return res.send('Can you hear me?');
 });
+
+// 라우트 예제입니다.
+
+app.use('/posts', _post2.default);
 
 var server = app.listen(port, function () {
   console.log('Express listening on port', port);
--- monica_react_test/server/src/server.js
+++ monica_react_test/server/src/server.js
@@ -8,13 +8,14 @@ let port = 4001;
 // 경로 '/' 로 들어오는 요청들은 public 폴더로 정적 라우팅합니다.
 app.use('/', express.static(__dirname + '/../public'));
 
-app.get('/api/hello', (req, res) => {
-  let data = [
-    { msg: 'what!' },
-    { msg: 'Welcome!' }
-  ];
-  res.send(data);
+app.get('/hello', (req, res) => {
+  return res.send('Can you hear me?');
 });
+
+// 라우트 예제입니다.
+import posts from './routes/post';
+app.use('/posts', posts);
+
 
 const server = app.listen(port, () => {
   console.log('Express listening on port', port);
